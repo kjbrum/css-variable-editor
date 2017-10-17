b04c1f877f61b0dcd5434cf82a60f08ef3a0c837
Kyle Brumm <kjbrumm@gmail.com>
2017-10-16 21:54:44 -0500

    Got a very basic version working

--- script.js
+++ script.js
@@ -1,44 +1,116 @@
-(function() {
-    var cve = {
-        // Initialize all the things
-        init: function() {
-            this.addJquery();
-            this.addStyles();
-            this.addEditor();
-        },
+var cve = {
+    // Initialize all the things
+    init: function() {
+        var self = this;
+        this.addJquery();
 
-        // Add jQuery to the head
-        addJquery: function() {
-            var s = document.createElement('script');
-                s.type = 'text/javascript';
-                s.src = 'https://code.jquery.com/jquery-3.2.1.min.js';
+        setTimeout(function() {
+            self.addStyles();
+            self.addEditor();
+            self.addListeners();
+        }, 500);
+    },
 
-            document.head.appendChild(s);
-        },
+    // Add jQuery to the head
+    addJquery: function() {
+        var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.src = 'https://code.jquery.com/jquery-3.2.1.min.js';
 
-        // Add our styling
-        addStyles = function() {
-            var self = this;
+        document.body.appendChild(s);
+    },
 
-            $('head').append('<style>\
-                .cve-bar {\
-                    position: absolute;\
-                    right: 0;\
-                    bottom: 0;\
-                    background: cyan;\
-                }\
-            </style>');
-        },
+    // Add our styling
+    addStyles: function() {
+        var self = this;
 
-        // Add the editor HTML
-        addEditor: function() {
-            var self = this;
+        $('head').append('<style>' +
+            '.cve {' +
+                'position: fixed;' +
+                'right: 0;' +
+                'bottom: 0;' +
+                'padding: 1rem;' +
+                'font-size: 0.9rem !important;' +
+                'color: black !important;' +
+                'background-color: white !important;' +
+            '}' +
+        '</style>');
+    },
 
-            $('html').append('<div class="cve">\
-                This should be awesome!\
-            </div>')
+    // Add the editor HTML
+    addEditor: function() {
+        var self = this;
+
+        $('body').append('' +
+        '<div class="cve">' +
+            '<div class="cve__input cve__input--color">' +
+                '<label>Background color:<br>' +
+                    '<input type="color" id="background-color" value="#ffffff"><br>' +
+                '</label>' +
+            '</div>' +
+            '<div class="cve__input cve__input--color">' +
+                '<label>Font color:<br>' +
+                    '<input type="color" id="font-color" value="#ffffff"><br>' +
+                '</label>' +
+            '</div>' +
+            '<div class="cve__input cve__input--text">' +
+                '<label>Font size:<br>' +
+                    '<input type="range" id="font-size" min="0.8" max="2" step="0.1" value="1"><br>' +
+                '</label>' +
+            '</div>' +
+            '<div class="cve__input cve__input--text">' +
+                '<button id="reset">Reset styles and restart</button>' +
+            '</div>' +
+        '</div>');
+    },
+
+    handleInput: function(property, value, unit) {
+        var unit = unit || '';
+        document.documentElement.style.setProperty('--'+property, value+unit);
+        localStorage.setItem(property, value+unit);
+    },
+
+    addListeners: function() {
+        var self = this;
+
+        $('#background-color').on('change', function(e) {
+            self.handleInput('background-color', e.target.value);
+        });
+
+        $('#font-color').on('change', function(e) {
+            self.handleInput('font-color', e.target.value);
+        });
+
+        $('#font-size').on('input', function(e) {
+            self.handleInput('font-size', e.target.value, 'rem');
+        });
+
+        $('#reset').on('click', function(e) {
+            localStorage.clear();
+            window.location.reload(true);
+        });
+
+        $(document).ready(function() {
+            self.updateFromLocalStorage('background-color');
+            self.updateFromLocalStorage('font-color');
+            self.updateFromLocalStorage('font-size');
+        });
+    },
+
+    updateFromLocalStorage: function(property) {
+        var value = localStorage.getItem(property);
+        var $input = $('#'+property);
+
+        if (value) {
+            document.documentElement.style.setProperty('--'+property, value);
+            $input.val(value.replace('rem', ''));
+        } else {
+            value = getComputedStyle(document.body).getPropertyValue('--'+property);
+            console.log(value);
+            $input.val(value.replace('rem', '').trim());
         }
     }
 
-    cve.init();
-});
+}
+
+cve.init();
